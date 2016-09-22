ae99efea979332d757a4c4eccdcd8fa83254baf0
Xander Griff <europealexander@yahoo.ca>
2016-09-22 19:40:06 +0000

    add color scheme

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'albums#create'
+  root 'albums#index'
   get 'albums/:id', to: 'albums#show'
   get 'albums', to: 'albums#index'
+  
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
